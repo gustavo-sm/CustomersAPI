113fcda0a0d1cb0810cb44e03bd7a0f45b56617a
gus <gustavo1sad@gmail.com>
2022-12-11 00:47:20 -0300

    Implementação de metodo de verificação para caso rota ja exista

--- src/routes/AppRouter.ts
+++ src/routes/AppRouter.ts
@@ -15,16 +15,27 @@ class AppRouter implements IAppRoutes{
         return this.app_router;
     }
     
-    public setRoutes(){
+    public setRoutes() : void {
         let routes_arr_length : number = this.routes.length;
+
         for(let i : number = 0; i<routes_arr_length; i++){
+            if(this.routeExists(this.routes[i].Path))
+                continue;
+
             if(this.routes[i].Method === 'get')
                 this.app_router.get(this.routes[i].Path, this.routes[i].task);
         }
     }
     
-    public routeExists() : boolean{
-        return true;
+    public routeExists(path_check : string) : boolean{
+        let routes_arr_length : number = this.app_router.stack.length;
+
+        for(let i : number = 0; i<routes_arr_length; i++){
+            if(this.app_router.stack[i].route.path === path_check)
+                return true;
+        }
+
+        return false;
     }
 
 }

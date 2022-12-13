f5c81c1bd396eeb2a19ab6f3f40eab639b44e80f
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-12-13 03:14:41 -0300

    add interface para conexão db

---
+++ src/models/interfaces/IDatabaseConnection.ts
@@ -0,0 +1,13 @@
+import { Connection } from "mysql";
+
+export interface IDatabaseConnection {
+    db_host : string;
+    db_name : string;
+    db_username : string;
+    db_password : string;
+    db_connection : Connection;
+    connect() : Connection;
+    query(query : string, conditions : string[]) : Array<object>
+    close() : void;
+
+}

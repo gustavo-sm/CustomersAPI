127613cc61ad89d8c0aba67dba244fae4b268571
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-12-13 03:49:12 -0300

    Removido redundancia na importação

--- src/models/interfaces/IDatabaseConnection.ts
+++ src/models/interfaces/IDatabaseConnection.ts
@@ -1,12 +1,12 @@
-import { Connection } from "mysql";
+import * as mysql from "mysql";
 
 export interface IDatabaseConnection {
     db_host : string;
     db_name : string;
     db_username : string;
     db_password : string;
-    db_connection : Connection;
-    connect() : Connection;
+    db_connection : mysql.Connection;
+    connect() : mysql.Connection;
     query(query : string, conditions : string[]) : Array<object>
     close() : void;
 

43ab1eb30c3702a0187c6708a9d661c0d75bfe36
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-12-13 04:55:48 -0300

    Correção retorno do metodo connect: Connection -> void

--- src/services/interfaces/IDatabaseConnection.ts
+++ src/services/interfaces/IDatabaseConnection.ts
@@ -1,12 +1,12 @@
-import * as mysql from "mysql";
+import { Connection } from "mysql";
 
 export interface IDatabaseConnection {
     db_host : string;
     db_name : string;
     db_username : string;
     db_password : string;
-    db_connection : mysql.Connection;
-    connect() : mysql.Connection;
+    db_connection : Connection;
+    connect() : void;
     query(query : string, conditions : string[]) : Array<object>
     close() : void;
 

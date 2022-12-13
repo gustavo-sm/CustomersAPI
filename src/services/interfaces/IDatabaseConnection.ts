203e0585fdb046a3d6fa77c242612a1ddd7ea606
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-12-13 04:57:03 -0300

    Correção de tipo aceito no db_connection para tipo generico

--- src/services/interfaces/IDatabaseConnection.ts
+++ src/services/interfaces/IDatabaseConnection.ts
@@ -1,11 +1,9 @@
-import { Connection } from "mysql";
-
-export interface IDatabaseConnection {
+export interface IDatabaseConnection <T> {
     db_host : string;
     db_name : string;
     db_username : string;
     db_password : string;
-    db_connection : Connection;
+    db_connection : T;
     connect() : void;
     query(query : string, conditions : string[]) : Array<object>
     close() : void;

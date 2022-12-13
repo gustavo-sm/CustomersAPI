d5a94f1cbbf2b340704d8af20445d51b0924705e
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-12-13 02:22:37 -0300

    Correções no index

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app : express.Express = express();
+app.disable('x-powered-by');
 
-const root_route : RootRoute = new RootRoute('/','get'),
-customers_route : CustomersRoute = new CustomersRoute('/customers/:custId?', 'get'),
+const root_route : RootRoute = new RootRoute('/'),
+customers_route : CustomersRoute = new CustomersRoute('/customers/:custId?'),
 app_router : AppRouter = new AppRouter([root_route, customers_route]);
 
 app_router.setRoutes();

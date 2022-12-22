d24c0101f5dc0bab7ef89b878650aa2b0aaed468
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-12-22 05:15:15 -0300

    Atualização implementação e abstração de conexão ao db

--- src/services/interfaces/IDatabaseConnection.ts
+++ src/services/interfaces/IDatabaseConnection.ts
@@ -1,10 +1,6 @@
 export interface IDatabaseConnection <TConnection, TPromise> {
-    db_host : string;
-    db_name : string;
-    db_username : string;
-    db_password : string;
     db_connection : TConnection;
-    connect() : void;
+    connect(db_host : string, db_name : string, db_username : string, db_password : string) : void;
     query(query : string, conditions : string[]) : Promise<TPromise>;
     close() : void;
 }

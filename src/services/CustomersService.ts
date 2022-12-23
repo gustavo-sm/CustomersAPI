5181984e453843ca8bfe5ff35aac69269d7ad4f3
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-12-23 14:51:27 -0300

    MVP CustomersService

--- src/services/CustomersService.ts
+++ src/services/CustomersService.ts
@@ -1,16 +1,24 @@
-import { Customers } from "../models/Customers";
+import { Customers } from "../models/Customers.js";
 import { IService } from "./interfaces/IService";
 import { MySqlDatabaseConnection } from "./MySqlDatabaseConnection";
 
-class CustomersService implements IService<MySqlDatabaseConnection, Customers> {
+class CustomersService implements IService <MySqlDatabaseConnection, Customers> {
     db_interface : MySqlDatabaseConnection;
 
     constructor(db_conn : MySqlDatabaseConnection){
         this.db_interface = db_conn;
     }
 
-    public async getAll() : Promise<Customers[]>{
-        return await this.db_interface.query("SELECT * FROM 01custdata",[]);
+    public async getAll() : Promise<Customers[]> {
+        const cust_arr : Customers[] = [],
+              results = await this.db_interface.query("SELECT * FROM 01custdata", []);
+
+        for(let i: number = 0; i< results.length; i++){
+            let cust_inst : Customers = new Customers(results[i].cust_id,results[i].cust_name, results[i].cust_calc);
+            cust_arr.push(cust_inst);
+        }
+        
+        return cust_arr;
     }
     public getById(cust_id : number) : Promise<Customers[]> {return;}
     

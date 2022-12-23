063c1311dfe38f9ffe827840e20c9f751c01c72a
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-12-23 14:52:07 -0300

    MVP CustomersController

--- src/controllers/CustomersController.ts
+++ src/controllers/CustomersController.ts
@@ -1,6 +1,30 @@
+import { CustomersService } from "../services/CustomersService.js";
+import { AppResponse } from "../models/AppResponse.js";
+import { MySqlDatabaseConnection } from "../services/MySqlDatabaseConnection.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
 class CustomersController {
-    public static getCustomers(cust_id? : string) : string {
-        return `${cust_id} -> param`;
+    public static getCustomer(cust_name? : string) : AppResponse {
+        //const cust_service : CustomersService = new CustomersService();
+        return;
+    }
+    public static async getCustomers() : Promise<AppResponse> {
+        const response : AppResponse = new AppResponse();
+        try {
+            const db_connection : MySqlDatabaseConnection = new MySqlDatabaseConnection(process.env.DB_HOST, process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
+            const cust_service : CustomersService = new CustomersService(db_connection);
+            
+            db_connection.connect();
+            response.createMessage(await cust_service.getAll(), 200);
+            db_connection.close();
+            return response;
+
+        } catch (err){
+            response.createMessage(err, 500);
+            return response;
+        }
+
     }
 }
 
